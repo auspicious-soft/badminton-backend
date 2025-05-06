b00cb6795eaee9ac5226a461ed956a10572a7088
Auspicious <info@auspicioussoft.com>
2025-05-06 16:31:53 +0530

    Fix Twilio client initialization and update password reset token message format

--- src/utils/sms/sms.ts
+++ src/utils/sms/sms.ts
@@ -2,14 +2,14 @@
 import twilio from "twilio";
 import { configDotenv } from "dotenv";
 configDotenv();
-const client = twilio(process.env.ACCOUNTSID as string, process.env.AUTHTOKEN as string);
-const twilioClient = twilio(process.env.TWILIO_ACCOUNT_SID!, process.env.TWILIO_AUTH_TOKEN!);
+const client = twilio(process.env.TWILIO_ACCOUNT_SID as string, process.env.TWILIO_AUTH_TOKEN as string);
+// const twilioClient = twilio(process.env.TWILIO_ACCOUNT_SID!, process.env.TWILIO_AUTH_TOKEN!);
 
 export const generatePasswordResetTokenByPhoneWithTwilio = async (phoneNumber: string ,token: string, expiresAt: Date) => {
 
   try {
     // Update message to reflect 2-minute expiry
-    const message = `Your password reset token is: ${token}. It is valid for 2 minutes.`;
+    const message = `You verification code from Play App is: ${token}. It is valid for 2 minutes.`;
     const res =  await client.messages.create({
         body: message,
         from: process.env.TWILIO_PHONE_NUMBER as string,
@@ -30,23 +30,23 @@ export const generatePasswordResetTokenByPhoneWithTwilio = async (phoneNumber: s
   }
 };
 
-export const generateOtpWithTwilio = async (phoneNumber: string, otp: string) => {
-  try {
-     const res= await twilioClient.messages.create({
-       body: `Your OTP is: ${otp}`,
-       from: `whatsapp:${process.env.TWILIO_PHONE_NUMBER}`,
-       to: `whatsapp:${phoneNumber}`,
-      });
-    return {
-      success: true,
-      message: "OTP is sent via Whatsapp",
-    };
-  } catch (error) {
-    console.error("Error sending otp  via Twilio:", error);
-    return {
-      success: false,
-      message: "Failed to send otp via Whatsapp",
-      error,
-    };
-  }
-};
+// export const generateOtpWithTwilio = async (phoneNumber: string, otp: string) => {
+//   try {
+//      const res= await twilioClient.messages.create({
+//        body: `Your OTP is: ${otp}`,
+//        from: `whatsapp:${process.env.TWILIO_PHONE_NUMBER}`,
+//        to: `whatsapp:${phoneNumber}`,
+//       });
+//     return {
+//       success: true,
+//       message: "OTP is sent via Whatsapp",
+//     };
+//   } catch (error) {
+//     console.error("Error sending otp  via Twilio:", error);
+//     return {
+//       success: false,
+//       message: "Failed to send otp via Whatsapp",
+//       error,
+//     };
+//   }
+// };
