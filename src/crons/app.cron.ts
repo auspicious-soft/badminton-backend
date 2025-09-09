6331f3df40e90f83a15efdae2e10c55585e1ecf0
Auspicious <info@auspicioussoft.com>
2025-09-09 16:05:37 +0530

    more updates

--- src/crons/app.cron.ts
+++ src/crons/app.cron.ts
@@ -53,7 +53,7 @@ export const startInvoiceCron = () => {
 };
 
 export const venueRainCron = async () => {
-  cron.schedule("30 * * * *", async () => {
+  cron.schedule("*/25 * * * *", async () => {
     try {
       console.warn("⚠️ Running venue rain update...");
       const now = new Date();
